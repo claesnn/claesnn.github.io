fc8faf6ac40ce2bfca2355656ed7f6ab8805721b
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-02 22:55:18 +0100

    Update baseUrl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseUrl: "claesnn.github.io",
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
 })
