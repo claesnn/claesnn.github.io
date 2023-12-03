593b6bfe54f712c3652516512a6d3f0f93c695a2
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-03 13:54:15 +0100

    Add Shadcn + socials icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next"
 import { Inter, Literata } from "next/font/google"
 import "./globals.css"
 import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Github, LinkedinIcon } from "lucide-react"
 
 const inter = Inter({ subsets: ["latin"] })
 const literata = Literata({ subsets: ["latin"] })
@@ -21,19 +23,38 @@ export default function RootLayout({
       <body className={inter.className + "min-h-screen pb-12"}>
         <header>
           <div className='border-b'>
-            <header className='p-5 max-w-6xl mx-auto flex items-center justify-between flex-wrap space-y-3'>
-              <div>
-                <h1 className={literata.className + " text-4xl font-bold"}>
-                  Claes Nymand Nilsson
-                </h1>
-                <p className='text-2xl'>Biotech & Software</p>
+            <header className='p-5 max-w-6xl mx-auto flex items-center flex-wrap space-y-3'>
+              <div className='mr-8'>
+                <Link href='/'>
+                  <h1 className={literata.className + " text-4xl font-bold"}>
+                    Claes Nymand Nilsson
+                  </h1>
+                  <p className='text-2xl'>Biotech & Software</p>
+                </Link>
               </div>
-              <nav className='space-x-4'>
-                <Link href='/'>Home</Link>
-                <Link href='/about'>About</Link>
-                <Link href='/projects'>Projects</Link>
-                <Link href='/contact'>Contact</Link>
+              <nav>
+                <Link href='/about'>
+                  <Button variant='ghost'>About</Button>
+                </Link>
+                <Link href='/projects'>
+                  <Button variant='ghost'>Projects</Button>
+                </Link>
+                <Link href='/contact'>
+                  <Button variant='ghost'>Contact</Button>
+                </Link>
               </nav>
+              <div className='flex ml-auto'>
+                <Link href='https://github.com/claesnn'>
+                  <Button variant='ghost'>
+                    <Github />
+                  </Button>
+                </Link>
+                <Link href='https://linkedin.com/in/claesnn'>
+                  <Button variant='ghost'>
+                    <LinkedinIcon />
+                  </Button>
+                </Link>
+              </div>
             </header>
           </div>
         </header>
