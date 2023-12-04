cfba36cf2d4c72df4a33f2a41dbba3533babfc44
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-04 23:23:16 +0100

    Added meta and title to fp and about

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+    <title>Claes Nymand Nilsson - Biotech & Software</title>
+    <meta name="description" content="Personal site of Claes Nymand Nilsson with a focus on Biotech, Software and Photography" />
+</svelte:head>
+
 <h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <a href="/about">About</a>
