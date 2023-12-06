00a94b2fc22672bb01927f81903a30f7159ec79e
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-06 20:55:44 +0100

    Revert "Added meta and title to fp and about"
    This reverts commit cfba36cf2d4c72df4a33f2a41dbba3533babfc44.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,2 @@
-<svelte:head>
-    <title>About - Claes Nymand Nilsson</title>
-    <meta name="description" content="About page for the site of Claes Nymand Nilsson with a focus on Biotech, Software and Photography" />
-</svelte:head>
-
 <p>Da blog</p>
 <a href="/">Home</a>
