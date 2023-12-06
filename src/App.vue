249b03a5ef464668c39e644626f6b675436f0422
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-06 20:55:44 +0100

    Revert "Add blog view"
    This reverts commit 33f2653dec80fa9aedc4927ea454c7156b9aef28.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router"
-import HelloWorld from "./components/HelloWorld.vue"
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
@@ -18,7 +13,6 @@ import HelloWorld from "./components/HelloWorld.vue"
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/blog">Blog</RouterLink>
       </nav>
     </div>
   </header>
