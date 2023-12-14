16e56eb5a131cec7a4a00eabee2369a968156a39
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 19:02:31 +0100

    Add NuxtImg

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -1,4 +1,8 @@
 <template>
   <h1 class="text-8xl font-bold text-center text-gray-800">My Header</h1>
-  
+  <NuxtImg
+    src="/images/DSC00741.jpg"
+    format="webp"
+    loading="lazy"
+    quality="80" />
 </template>
