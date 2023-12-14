a340869eeaf818b76e6d35acf535cf87da920b60
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 19:17:50 +0100

    Resize to smaller widths

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -1,23 +1,25 @@
 <template>
   <h1 class="text-8xl font-bold text-center text-gray-800">My Header</h1>
-  <NuxtImg
-    src="/images/DSC00741.jpg"
-    format="avif,webp"
-    loading="lazy"
-    quality="80" />
-  <NuxtImg
-    src="/images/DSC00787.jpg"
-    format="avif,webp"
-    loading="lazy"
-    quality="80" />
-  <NuxtImg
-    src="/images/DSC00888.jpg"
-    format="avif,webp"
-    loading="lazy"
-    quality="80" />
-  <NuxtImg
-    src="/images/DSC00892.jpg"
-    format="avif,webp"
-    loading="lazy"
-    quality="80" />
+  <div class="grid grid-cols-3 gap-3">
+    <NuxtImg
+      src="/images/DSC00741.jpg"
+      format="webp"
+      loading="lazy"
+      width="500" />
+    <NuxtImg
+      src="/images/DSC00787.jpg"
+      format="webp"
+      loading="lazy"
+      width="500" />
+    <NuxtImg
+      src="/images/DSC00888.jpg"
+      format="webp"
+      loading="lazy"
+      width="500" />
+    <NuxtImg
+      src="/images/DSC00892.jpg"
+      format="webp"
+      loading="lazy"
+      width="500" />
+  </div>
 </template>
