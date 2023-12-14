78d1b86c7af2785747aa1fdcf739ef1716c0ea24
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 20:00:17 +0100

    Add titles and descriptions

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Biotech, Photograhy & Software | Claes Nymand Nilsson",
+  description:
+    "Biotech, Photograhy & Software. Personal site of Claes Nymand Nilsson",
+})
+</script>
+
 <template>
   <h1 class="text-5xl lg:text-8xl font-bold text-center text-gray-800 mb-8">
     Home
--- pages/photography.vue
+++ pages/photography.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Photography | Claes Nymand Nilsson",
+  description: "Photographies from Claes Nymand Nilsson",
+})
+</script>
+
 <template>
   <MyHeader />
 </template>
