5d21530b0eae083b4a21f0fdb81fc7c6eb886854
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 18:36:32 +0100

    Add tailwind css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"]
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
 })
