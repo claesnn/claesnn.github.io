619792a7457e85b6e40887291eda750fd9dde2ba
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 19:41:04 +0100

    Add responsiveness

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -1,25 +1,29 @@
 <template>
   <h1 class="text-8xl font-bold text-center text-gray-800 mb-8">Photography</h1>
-  <div class="grid grid-cols-3 gap-3">
+  <div class="grid md:grid-cols-2 xl:grid-cols-3 gap-3 max-w-7xl mx-auto">
     <NuxtImg
       src="/images/DSC00741.jpg"
       format="webp"
       loading="lazy"
-      width="500" />
+      class="mx-auto"
+      width="600" />
     <NuxtImg
       src="/images/DSC00787.jpg"
       format="webp"
       loading="lazy"
-      width="500" />
+      class="mx-auto"
+      width="600" />
     <NuxtImg
       src="/images/DSC00888.jpg"
       format="webp"
       loading="lazy"
-      width="500" />
+      class="mx-auto"
+      width="600" />
     <NuxtImg
       src="/images/DSC00892.jpg"
       format="webp"
+      class="mx-auto"
       loading="lazy"
-      width="500" />
+      width="600" />
   </div>
 </template>
