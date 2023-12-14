db4d4ad4712bacdbe61133c71ef783ec99ff1d39
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 16:08:37 +0100

    Add counter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from "@/stores/counter"
+import TheWelcome from "../components/TheWelcome.vue"
+const count = useCounterStore()
 </script>
 
 <template>
   <main>
+    <button
+      class="m-5 p-3 bg-slate-200 rounded"
+      @click="count.increment()">
+      Increment ({{ count.count }})
+    </button>
     <TheWelcome />
   </main>
 </template>
