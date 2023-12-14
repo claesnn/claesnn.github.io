8b99dc396d30b5e88ad8aa458d63b11dda0f64ac
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 20:04:04 +0100

    fix max-w typo

--- app.vue
+++ app.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="">
-    <nav class="mb-4 border-b p-5 bg-slate-50">
-      <div class="mx-w-7xl mx-auto space-x-4">
-        <NuxtLink to="/">Home</NuxtLink>
-        <NuxtLink to="/photography">Photography</NuxtLink>
-      </div>
-    </nav>
-    <main class="p-5">
-      <NuxtPage />
-    </main>
-  </div>
+  <nav class="mb-4 border-b p-5 bg-slate-50">
+    <div class="max-w-7xl mx-auto space-x-4">
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/photography">Photography</NuxtLink>
+    </div>
+  </nav>
+  <main class="p-5">
+    <NuxtPage />
+  </main>
 </template>
