0b6c2e4d0da13a04c44a66cd7dea5958b2215ab0
Claes Nymand Nilsson <claes89@gmail.com>
2023-12-14 19:09:35 +0100

    Add 4 images

--- components/MyHeader.vue
+++ components/MyHeader.vue
@@ -2,7 +2,22 @@
   <h1 class="text-8xl font-bold text-center text-gray-800">My Header</h1>
   <NuxtImg
     src="/images/DSC00741.jpg"
-    format="webp"
+    format="avif,webp"
+    loading="lazy"
+    quality="80" />
+  <NuxtImg
+    src="/images/DSC00787.jpg"
+    format="avif,webp"
+    loading="lazy"
+    quality="80" />
+  <NuxtImg
+    src="/images/DSC00888.jpg"
+    format="avif,webp"
+    loading="lazy"
+    quality="80" />
+  <NuxtImg
+    src="/images/DSC00892.jpg"
+    format="avif,webp"
     loading="lazy"
     quality="80" />
 </template>
