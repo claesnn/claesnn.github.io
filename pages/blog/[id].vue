ffb16e661f8900735142fb22b597f657e1dddd1e
Claes Nymand Nilsson <claes89@gmail.com>
2024-01-20 20:43:14 +0100

    Add date to blog page

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
 const route = useRoute()
 const blog = route.params.id
+
+function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })
+}
 </script>
 
 <template>
@@ -10,6 +18,9 @@ const blog = route.params.id
         <h1 class="font-[Kurale] xl:text-8xl lg:text-6xl text-4xl mb-4">
           {{ doc.title }}
         </h1>
+        <span class="uppercase text-sm text-slate-400">
+          {{ formatDate(doc.createdAt) }}
+        </span>
         <h2
           class="font-[Kurale] xl:text-3xl lg:text-2xl text-xl mb-4 text-slate-700">
           {{ doc.description }}
